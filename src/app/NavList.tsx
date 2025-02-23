19d19bdb4c7d773619bd5f97792ae2e5819e8b6b
Avinash <avihsan8922@gmail.com>
2025-02-23 10:42:48 +0530

    Update NavList.tsx

--- src/app/NavList.tsx
+++ src/app/NavList.tsx
@@ -11,7 +11,7 @@ import selectedNavStore from '../state/external/nav';
     LanguageComponent
 */
 export default function NavList(){
-    const selected = useSyncExternalStore(selectedNavStore.subscribe, selectedNavStore.getSnapshot);
+    const selected = useSyncExternalStore(selectedNavStore.subscribe, selectedNavStore.getSnapshot, selectedNavStore.getServerSnapshot);
     const navListUI = ({id, name}: {id : string, name: string}) => {
         const isSelected = selected == id;
         return (<a key={id} href={id} className={isSelected ? 'text-foreground' : 'text-secondary'}><span className='text-primary'>#</span>{name}</a>);
